3d83495f77440c9052ef7cb217d3cae0301462ca
Ender Bonnet <enbonnet@gmail.com>
2019-11-06 18:57:54 -0300

    add slugify config gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,55 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
+const { slugify } = require("./src/utils/slugify")
 
-// You can delete this file if you're not using it
+const makeRequest = (graphql, request) =>
+  new Promise((resolve, reject) => {
+    // Query for nodes to use in creating pages.
+    resolve(
+      graphql(request).then(result => {
+        if (result.errors) {
+          reject(result.errors)
+        }
+
+        return result
+      })
+    )
+  })
+
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const getArticles = makeRequest(
+    graphql,
+    `
+    {
+      allStrapiReceta(sort: {order: ASC, fields: strapiId}) {
+        edges {
+          node {
+            strapiId
+            titulo
+          }
+        }
+      }
+    }
+    `
+  ).then(result => {
+    // Create pages for each receta.
+    console.info("🍔 Creando recetas")
+    result.data.allStrapiArticle.edges.forEach(({ node }) => {
+      console.info(`${node.strapiId} - ${node.titulo}`)
+      createPage({
+        path: `receta/${node.strapiId}/${slugify(node.titulo)}`,
+        component: path.resolve(`src/templates/receta.js`),
+        context: {
+          id: node.strapiId,
+          titulo: node.titulo,
+        },
+      })
+    })
+  })
+
+  // Query for articles nodes to use in creating pages.
+  return getArticles
+}
